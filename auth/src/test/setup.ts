6f566b0d47c90af45d594b1a20582135bda681ac
Katherine Ebel <kathyebel@me.com>
2020-07-09 18:42:12 -0400

    tests for auth current-user

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,29 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
 import { app } from '../app'
+import request from 'supertest'
 
 let mongo: MongoMemoryServer
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup(): Promise<string[]>
+    }
+  }
+}
+
+global.signup = async () => {
+  const email = `test@test.com`
+  const password = `password`
+
+  const response = await request(app)
+    .post(`/api/users/signup`)
+    .send({ email, password })
+    .expect(201)
+  return response.get('Set-Cookie')
+
+}
 
 beforeAll(async () => {
   mongo = new MongoMemoryServer()
